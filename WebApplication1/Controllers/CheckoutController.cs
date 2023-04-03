cf613620d6a88a4ed4ad38a77e4e6a3f3cdbd9ab
thduc2806 <thduc.2000@gmail.com>
2023-04-03 18:01:33 +0700

    paypal script

--- WebApplication1/Controllers/CheckoutController.cs
+++ WebApplication1/Controllers/CheckoutController.cs
@@ -38,83 +38,112 @@ namespace WebApplication1.Controllers
 			var result = await _cartApi.GetCart(userId);
 			decimal totalPrice = result.Sum(x => x.Price * x.Quantity);
 			ViewBag.TotalPrice = totalPrice;
-			return View(result);
+            ViewBag.TotalPriceUsd = result.Sum(p => p.Quantity * Math.Round( p.Price/ RangeRate, 2));
+            return View(result);
 		}
-		//public async Task<IActionResult> PayPayPayment()
-		//{
-		//	var config = new Dictionary<string, string>
-		//	{
-		//		{ "clientId", _clientId },
-		//		{ "clientSecret", _secrecKey },
-		//		{ "mode", _mode }
-		//	};
+		public async Task<IActionResult> PayPalPayment()
+		{
+			var config = new Dictionary<string, string>
+			{
+				{ "clientId", _clientId },
+				{ "clientSecret", _secrecKey },
+				{ "mode", _mode }
+			};
 
-		//	var accessToken = new OAuthTokenCredential(config).GetAccessToken();
-		//	var apiContext = new APIContext(accessToken);
+			var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+			var apiContext = new APIContext(accessToken);
 
-		//	var itemList = new ItemList()
-		//	{
-		//		items = new List<Item>()
-		//	};
-		//	var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-		//	var cart = await _cartApi.GetCart(userId);
-		//	var total = Math.Round(cart.Sum(p => p.Quantity * p.Price) / RangeRate, 2);
+			var itemList = new ItemList()
+			{
+				items = new List<Item>()
+			};
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+			var cart = await _cartApi.GetCart(userId);
+			var total = Math.Round(cart.Sum(p => p.Quantity * p.Price) / RangeRate, 2);
+
+			foreach (var item in cart)
+			{
+				itemList.items.Add(new Item()
+				{
+					name = item.Name,
+					currency = "USD",
+					price = Math.Round((item.Price) / RangeRate, 2).ToString(),
+					quantity = item.Quantity.ToString(),
+					sku = "sku",
+				});
+			}
+			var hostname = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+			var payment = new Payment()
+			{
+				intent = "sale",
+				transactions = new List<Transaction>()
+				{
+					new Transaction()
+					{
+						amount = new Amount()
+						{
+							total = total.ToString(),
+							currency = "USD",
+							
+						},
+						item_list = itemList,
+						description = "Invoice",
+						invoice_number = Guid.NewGuid().ToString(),
+					}
+				},
+				redirect_urls = new RedirectUrls()
+				{
+					cancel_url = $"{hostname}/checkout/paymentfail",
+					return_url = $"{hostname}/checkout/paymentsuccess",
+				},
+				payer = new Payer()
+				{
+					payment_method = "paypal",
+				}
+			};
+
+			var createPayment = payment.Create(apiContext);
+			var redirectUrl = createPayment.links.FirstOrDefault(l => l.rel.Equals("approval_url"));
+
+			if (redirectUrl != null)
+			{
+
+				return Redirect(redirectUrl.href);
+			}
+			else
+			{
+				return Redirect("/checkout/paymentfail");
+			}
+
+		}
+
+		public IActionResult PayPalReturn(string paymentId, string token, string payerId)
+		{
+            var config = new Dictionary<string, string>
+            {
+                { "clientId", _clientId },
+                { "clientSecret", _secrecKey },
+                { "mode", _mode }
+            };
+
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+
+			var paymentExecution = new PaymentExecution
+			{
+				payer_id = payerId,
+			};
+
+			var payment = new Payment
+			{
+				id = payerId
+			};
+
+			var executedPayment = payment.Execute(apiContext, paymentExecution);
 			
-		//	foreach (var item in cart)
-		//	{
-		//		itemList.items.Add(new Item()
-		//		{
-		//			name = item.Name,
-		//			currency = "USD",
-		//			price = Math.Round((item.Price) / RangeRate, 2).ToString(),
-		//			quantity = item.Quantity.ToString(),
-		//			sku = "sku",
-		//			tax = "0",
-		//		});
-		//	}
-		//	var paypalOrderId = DateTime.Now.Ticks;
-		//	var hostname = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
-		//	var payment = new Payment()
-		//	{
-		//		intent = "sale",
-		//		transactions = new List<Transaction>()
-		//		{
-		//			new Transaction()
-		//			{
-		//				amount = new Amount()
-		//				{
-		//					total = total.ToString(),
-		//					currency = "USD",
-		//				},
-		//				item_list = itemList,
-		//				description = $"Invoice #{paypalOrderId}",
-		//				invoice_number = paypalOrderId.ToString(),
-		//			}
-		//		},
-		//		redirect_urls = new RedirectUrls()
-		//		{
-		//			cancel_url = $"{hostname}/checkout/paymentfail",
-		//			return_url = $"{hostname}/checkout/paymentsuccess",
-		//		},
-		//		payer = new Payer()
-		//		{
-		//			payment_method = "paypal",
-		//		}
-		//	};
+			return Json(executedPayment);
 
-		//	var createPayment = payment.Create(apiContext);
-		//	var redirectUrl = createPayment.links.FirstOrDefault(l => l.rel.Equals("approval_url"));
-
-		//	if(redirectUrl != null)
-		//	{
-		//		return Redirect(redirectUrl.href);
-		//	}
-		//	else
-		//	{
-		//		return Redirect("/checkout/paymentfail");
-		//	}	
-			
-		//}
+        }
 
 	}
 }
